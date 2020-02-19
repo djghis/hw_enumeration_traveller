76df0f7f0d41df1f1840e520eecf27170830bb50
djghis <djghis@hotmail.com>
2020-02-19 17:13:57 +0000

    mvp and 1 extension done

--- models/traveller.js
+++ models/traveller.js
@@ -12,23 +12,33 @@ Traveller.prototype.getJourneyEndLocations = function () {
   return this.journeys.map((journey) => {
       return journey.endLocation;
   });
-};;
+};
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
-};
+  return this.journeys.filter((journey) => {
+      return journey.transport === transport;
+    });
+  };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
-};
+  return this.journeys.filter((journey) => {
+      return journey.distance >= minDistance;
+    });
+  };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+      return total += journey.distance;
+    }, 0);
+  };
 
-};
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
-};
+  // map thru array and then return unique value
+  return this.journeys.reduce((total, journey) => {
+      return total += journey.transport;
+    }, []);
+  };
 
 
 module.exports = Traveller;
